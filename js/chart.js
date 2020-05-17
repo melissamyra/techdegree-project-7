0db15526ef23fa9ea5dcb485be1afe238c58bb13
melissamyra <35929806+melissamyra@users.noreply.github.com>
2020-05-18 05:58:00 +0800

    Added charts

---
+++ js/chart.js
@@ -0,0 +1,116 @@
+let tChart = document.getElementById('trafficChart');
+let dChart = document.getElementById('dailyChart');
+let mChart = document.getElementById('mobileChart');
+
+// Global options =====================>
+Chart.defaults.global.defaultFontFamily = 'Open Sans';
+Chart.defaults.global.defaultFontSize = 8;
+Chart.defaults.global.defaultFontColor = '#8a8a8a';
+
+
+// Data Sets ==========================>
+let monthlyTraffic = {
+    backgroundColor: 'rgba(67, 139, 129, 0.2)',
+    lineTension: 0,
+    borderColor: '#5dd2d6',
+    pointRadius: 4.5,
+    pointBackgroundColor: '#fff',
+    borderJoinStyle: 'miter',
+    borderWidth: 1,
+    hoverBorderWidth: 2,
+    hoverBorderColor: '#009d9d',
+    data: [
+      1000, 1500,
+      1250, 1750,
+      2000, 1500,
+      1500, 1000,
+      1750, 2250],
+};
+
+
+// Traffic LINE Chart ==================>
+let trafficChart = new Chart(tChart, {
+    type: 'line',
+    data: {
+        labels: [
+          '16-22', '23-29',
+          '30-5', '6-12',
+          '13-19', '20-26',
+          '27-3', '4-10',
+          '11-17', '18-24'],
+        datasets: [monthlyTraffic]
+    },
+            options: {
+              legend: {
+                display: false
+              },
+              scales: {
+                gridLines: {
+                  color: '#fff'
+                },
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+                },
+            }
+});
+
+// Daily Traffic BAR Chart
+let myBarChart = new Chart(dChart, {
+    type: 'bar',
+    data: {
+      labels: [
+        'S', 'M',
+        'T', 'W',
+        'T', 'F',
+        'S'],
+      datasets: [{
+          backgroundColor: '#84dadd',
+          lineTension: 0,
+          pointRadius: 4.5,
+          pointBackgroundColor: '#fff',
+          borderJoinStyle: 'miter',
+          borderWidth: 0,
+          data: [
+            50, 75,
+            150, 100,
+            190, 175,
+            75],
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      }
+    }
+});
+
+// Mobile DOUGHNUT Chart
+var mobileChart = new Chart(mChart, {
+    type: 'doughnut',
+    data: {
+      labels: [
+        'Phones',
+        'Tablets',
+        'Desktop'],
+      datasets: [{
+        backgroundColor: [
+        '#906cd4',
+        '#3bd5b0',
+        '#84dadd'],
+        data: [
+          10,
+          10,
+          80],
+        }]
+    },
+    options: {
+      legend: {
+        position: 'right',
+        boxWidth: 40,
+        fontSize: 12,
+      }
+    }
+});
--- js/script.js
+++ js/script.js
@@ -1,37 +1,10 @@
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    lineTension : 0,
-    borderColor: '#5dd2d6',
-    data: {
-        labels: [
-          '16-22', '23-29',
-          '30-5', '6-12',
-          '13-19', '20-26',
-          '27-3', '4-10',
-          '11-17', '18-24'],
-        datasets: [{
-            label: 'Weekly',
-            data: [
-              1000, 1500,
-              1250, 1750,
-              2000, 1500,
-              1500, 1000,
-              1750, 2250],
-            backgroundColor: 'rgba(67, 139, 129, 0.2)',
-            borderColor: '#5dd2d6',
-            pointRadius: 4.5,
-            pointBackgroundColor: '#fff',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+let trafficList = document.querySelector('.dashboard__traffic ul');
+
+trafficList.addEventListener('click', (e) => {
+
+  if (e.target.tagName === 'LI') {
+
+    
+  }
+
 });
